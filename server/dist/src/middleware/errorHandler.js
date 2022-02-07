df744bfc22be182de4bae5f146dc38ff4c476db4
MansonL <mansonlautaro@gmail.com>
2022-02-06 21:06:50 -0300

    fix: dist folder deleted, .gitignore changed.

--- server/dist/src/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-const logger_1 = require("../services/logger");
-const errorHandler = (err, req, res, next) => {
-    logger_1.logger.error(`Error: ${err.error}. Message: ${err.message}. Stack: ${err.stack}`);
-    res.status(err.error).send(err.message);
-};
-exports.errorHandler = errorHandler;
