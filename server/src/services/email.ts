40ed39e6c94927be195f4b489635f242a0131ee5
Manson <mansonlautaro@gmail.com>
2022-02-09 19:23:46 -0300

    fix: google auth library replaced with googleapis for nodemailer

--- server/src/services/email.ts
+++ server/src/services/email.ts
@@ -1,26 +1,27 @@
-import { OAuth2Client } from "google-auth-library"
-import { Config } from "../config/config"
-import { createTransport, SentMessageInfo, Transporter } from 'nodemailer'
+import { google } from 'googleapis';
+import { Config } from "../config/config";
+import { createTransport, SentMessageInfo, Transporter } from 'nodemailer';
 import { GaxiosError, GaxiosResponse } from "gaxios";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
 import { ApiError } from "../api/errorApi";
+import { OAuth2Client } from 'google-auth-library';
 
 
 //                            GMAIL NODEMAILER IMPLEMENTATION
 
 export const createTransporter : () => Promise<Transporter<SentMessageInfo> | ApiError> = async () => {
-    const oauth2Client = new OAuth2Client({
+    const oAuth2Client : OAuth2Client = new google.auth.OAuth2({
         clientId: Config.GOOGLE_CLIENT_ID,
         clientSecret: Config.GOOGLE_CLIENT_SECRET,
         redirectUri: "https://developers.google.com/oauthplayground",
     });
 
-    oauth2Client.setCredentials({
+    oAuth2Client.setCredentials({
         refresh_token: Config.GOOGLE_REFRESH_TOKEN,
     });
 
     const accessToken : string | ApiError = await new Promise((resolve, reject) => {
-        oauth2Client.getAccessToken((err: GaxiosError | null, token?: string | null, res?: GaxiosResponse | null) => {
+        oAuth2Client.getAccessToken((err: GaxiosError | null, token?: string | null, res?: GaxiosResponse | null) => {
             if(err) reject(new ApiError(err.code ? Number(err.code) : 400, err.message));
             if(token) resolve(token);
         })
