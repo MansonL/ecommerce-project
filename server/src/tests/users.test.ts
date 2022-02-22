9a9cf1a8e28a5d3adcbb7b0fd5924a2311d9aa4d
MansonL <mansonlautaro@gmail.com>
2022-02-22 09:00:52 -0300

    test: continue creating user tests.

--- server/src/tests/users.test.ts
+++ server/src/tests/users.test.ts
@@ -1,9 +1,14 @@
-import fetch from "node-fetch";
-import { Config, hostURL } from "../config/config";
-import { UserInfo } from '../interfaces/users'
+/* eslint-disable no-unused-vars*/
 
-const botData : UserInfo = {
-  username: Config.GOOGLE_EMAIL,
+import moment from "moment";
+import { Types } from "mongoose";
+import fetch, { Response } from "node-fetch";
+import { hostURL } from "../config/config";
+import { UserInfo } from "../interfaces/users";
+import { logger } from "../services/logger";
+
+const testUserData: UserInfo = {
+  username: "test@gmail.com",
   password: "test123",
   repeatedPassword: "test123",
   name: `Ecommerce`,
@@ -15,24 +20,78 @@ const botData : UserInfo = {
   isAdmin: false,
 };
 
+const testStoredUserData = {
+  createdAt: moment().format(),
+  modifiedAt: moment().format(),
+  _id: new Types.ObjectId(),
+};
+
 const defaultUserURL = `${hostURL}/users`;
 
+const mockedFetch = fetch as jest.MockedFunction<typeof fetch>;
 
-describe('Users API Tests', () => {
-  it("POST users", async () => {
-    try{
-      const adminPostResponse  = await fetch(`${defaultUserURL}/save`, {
-        body: JSON.stringify(botData),
-        method: 'POST',
-        headers : { 'Content-Type': 'application/json'},
-      });
-      console.log(await adminPostResponse.json())
-    }catch (error) {
-      console.log(error)
-    }
-  })
-})
+describe("Users API Tests", () => {
+  describe("POST save user", () => {
+    it("user succesfully saved", async () => {
+      try {
+        mockedFetch.mockImplementationOnce(
+          (url, requestOptions) =>
+            new Promise<Response>((resolve, reject) =>
+              setTimeout(
+                () =>
+                  resolve(
+                    new Response(
+                      JSON.stringify({
+                        message: "User successfully created.",
+                        data: { ...testStoredUserData, ...testUserData },
+                      }),
+                      {
+                        url: url.toString(),
+                        status: 201,
+                        statusText: "Created",
+                      }
+                    )
+                  ),
+                1
+              )
+            )
+        );
 
+        const response = await mockedFetch(`${defaultUserURL}/save`, {
+          body: JSON.stringify(testUserData),
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        const responseJSON = await response.json();
+        expect(response.status).toBe(201);
+        expect(response.statusText).toBe("Created");
+        expect(responseJSON).toEqual({
+          message: "User successfully created.",
+          data: { ...testStoredUserData, ...testUserData },
+        });
+
+        /*
+        const response: Response = await fetch(`${defaultUserURL}/save`, {
+          body: JSON.stringify(botData),
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        const responseJSON = await response.json();
+        console.log(responseJSON);
+        console.log(response.headers);
+        */
+      } catch (error) {
+        logger.warn(error);
+      }
+    });
+  });
+});
 
 /*
 test("GET users", async () => {
