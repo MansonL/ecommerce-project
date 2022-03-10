d8934539a86eb045d46e6202f7aee76522e0e9c9
MansonL <mansonlautaro@gmail.com>
2022-03-10 09:08:38 -0300

    feature: some ui fixes. orders responsive ui not completed.

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './home.css';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./home.css";
 
+export function Home() {
+  const navigate = useNavigate();
 
-export function Home () {
-    
-    const navigate = useNavigate();
+  console.log(
+    `width: ${window.window.outerWidth}. height: ${window.window.outerHeight}`
+  );
 
-    return(
-        <React.Fragment>
-       <section className="body-container home-background">
-<div className="welcome-text">
-    <p className="first-text">Welcome to my first</p>
-    <p className="second-text">Ecommerce Project </p>
-  </div>
-   <button className="welcome-products-btn" onClick={() => navigate('../products')}>Start looking</button>
-</section>
+  return (
+    <React.Fragment>
+      <section className="body-container home-background">
+        <div className="welcome-text">
+          <p className="first-text">Welcome to my first</p>
+          <p className="second-text">Ecommerce Project </p>
+        </div>
+        <button
+          className="welcome-products-btn"
+          onClick={() => navigate("../products")}
+        >
+          Start looking
+        </button>
+      </section>
     </React.Fragment>
-    )
+  );
 }
